bb8b428169f7f87de63267694b6f749f2799756e
Shital Bhagat <shitalbhagat23@navgurukul.org>
2025-01-15 16:21:09 +0530

    deploy

--- index.js
+++ index.js
@@ -21,9 +21,3 @@ mongoose.connect(process.env.MONGO_URI)
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
 });
-
-app.get('/', (req, res) => {
-    res.send('Hello World');
-}
-);
-//const run = require('./geminiApi');
